03c4fe5c5e80a88946d43714385982c931f28171
thomaspapachristou <58434483+thomaspapachristou@users.noreply.github.com>
2021-08-30 16:34:45 +0200

    Add class lib

---
+++ ClassLibrary1/ClassLibrary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class ClassLib
+    {
+
+        public static int Addition(int intTarget, int n)
+        {
+            return intTarget + n;
+        }
+
+        public static int Square(int intTarget, int n)
+        {
+            return intTarget - n;
+        }
+
+        public static int Multiplication(int intTarget, int n)
+        {
+            return intTarget * n; 
+        }
+
+        public static int Division(int intTarget, int n)
+        {
+            
+            if (intTarget == 0 || n == 0)
+            {
+                throw new DivideByZeroException("You can't divide by 0");
+            }
+
+            return intTarget / n;
+        }
+
+    }
+}
